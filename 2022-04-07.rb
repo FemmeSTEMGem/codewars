1a66a5ba766596661e0e4f277cab8ad85f99a200
FemmeSTEMGem <femmestemgem@gmail.com>
2022-04-07 19:23:51 -0400

    2022-04-07 Ruby

---
+++ 2022-04-07.rb
@@ -0,0 +1,16 @@
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+def basic_op(operator, value1, value2)
+  eval [value1, operator, value2].join
+end
+
+puts basic_op('+', 4, 7)
