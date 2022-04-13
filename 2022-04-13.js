03ad77bac930177d9b796788ea310a0a065bfad4
FemmeSTEMGem <femmestemgem@gmail.com>
2022-04-13 15:22:52 -0400

    2022-04-13 JavaScript

---
+++ 2022-04-13.js
@@ -0,0 +1,21 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+  classPoints.push(yourPoints)
+  const classPointsAvg = classPoints.reduce((previousValue, currentValue) => previousValue + currentValue, 0) / classPoints.length
+  if (yourPoints > classPointsAvg) {
+    return true
+  }
+  return false
+}
+
+console.log(betterThanAverage([2, 3], 5))
