34296bb04eab55b96a4522a81a75fdcab0735f89
FemmeSTEMGem <femmestemgem@gmail.com>
2022-12-03 22:59:22 -0500

    Daily fundamentals practice

--- 2022-12/2022-12-03.js
+++ 2022-12/2022-12-03.js
@@ -1,10 +1,10 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-// function evenOrOdd(number) {
-//   return number % 2 ? "Even" : "Odd"
-// }
+function evenOrOdd(number) {
+  return number % 2 ? "Even" : "Odd"
+}
 
-// console.log(evenOrOdd(7))
+console.log(evenOrOdd(7))
 
 
 // This code does not execute properly. Try to figure out why.
@@ -14,3 +14,20 @@ function multiply(a, b){
 }
 
 console.log(multiply(2, 5))
+
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  sum = 0
+  for (const num of arr) {
+    num >= 0 ? sum += num : sum += 0
+  }
+  return sum
+}
+
+console.log(positiveSum([1,-4,7,12]))
