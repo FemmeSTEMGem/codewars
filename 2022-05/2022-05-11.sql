4256f3f6fdcbee81a38e74c082ba2f6d77138f2d
FemmeSTEMGem <femmestemgem@gmail.com>
2022-12-03 21:12:00 -0500

    Deleting empty files

---
+++ 2022-05/2022-05-11.sql
@@ -0,0 +1,7 @@
+-- Return a table with a single column named Greeting with the phrase 'hello world!'
+
+-- Please use Data Manipulation Language and not Data Definition Language to solve this Kata
+
+-- write your select statement to return hello world
+
+SELECT 'hello world!' as Greeting;
