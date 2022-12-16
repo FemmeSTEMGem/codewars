d02ff4bebed61fa5d2fbe3a21a1c2fdc1e1252fd
FemmeSTEMGem <femmestemgem@gmail.com>
2022-12-16 18:29:35 -0500

    Daily fundamentals practice

--- 2022-12/2022-12-16.js
+++ 2022-12/2022-12-16.js
@@ -14,3 +14,20 @@ function basicOp(operation, value1, value2) {
 }
 
 console.log(basicOp("+", 4, 7))
+
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+  return name.split(" ")[0][0].toUpperCase() + "." + name.split(" ")[1][0].toUpperCase()
+}
+
+console.log(abbrevName("sam harris"))
