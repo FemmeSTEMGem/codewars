a28af6c534408f00a4d833d040a994b09fd2a2e0
femmestemgem <femmestemgem@gmail.com>
2023-02-22 14:42:19 -0500

    Daily fundamentals practice

--- 2023-02/2023-02-22.js
+++ 2023-02/2023-02-22.js
@@ -72,4 +72,18 @@ function countPositivesSumNegatives(input) {
   return newArray
 }
 
-console.log(countPositivesSumNegatives(null))
+//? console.log(countPositivesSumNegatives(null))
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+// 
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// 
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  return array.map(element => -element)
+}
+
+//? console.log(invert([]))
