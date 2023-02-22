6164eabf1b2ad23a9bab5915fd6bdd8784e802ad
femmestemgem <femmestemgem@gmail.com>
2023-02-22 15:47:03 -0500

    Daily fundamentals practice

--- 2023-02/2023-02-22.js
+++ 2023-02/2023-02-22.js
@@ -117,3 +117,15 @@ function fakeBin(x){
 }
 
 //? console.log(fakeBin("12345678"))
+
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  return x.reduce((a, b) => a * b)
+}
+
+//? console.log(grow([1, 2, 3, 4]))
+
+
