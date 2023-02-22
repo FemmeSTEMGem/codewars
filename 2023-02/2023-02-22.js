a1ee2f110da4f57c48cb92d05b9841214e012721
femmestemgem <femmestemgem@gmail.com>
2023-02-22 15:39:07 -0500

    Daily fundamentals practice

--- 2023-02/2023-02-22.js
+++ 2023-02/2023-02-22.js
@@ -107,3 +107,13 @@ function findAverage(array) {
 
 //? console.log(findAverage([]))
 
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+// 
+// Note: input will never be an empty string
+
+function fakeBin(x){
+  return x.split('').map(element => Math.round(element < 5 ? 0 : 1)).join('')
+}
+
+//? console.log(fakeBin("12345678"))
