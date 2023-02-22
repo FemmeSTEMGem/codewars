2b97223c5e45d1cf2bb4264e8ccf0285b32ec208
femmestemgem <femmestemgem@gmail.com>
2023-02-22 11:24:14 -0500

    Daily fundamentals practice

--- 2023-02/2023-02-22.js
+++ 2023-02/2023-02-22.js
@@ -6,4 +6,32 @@ function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
 
-console.log(greet("Shannon"))
+//? console.log(greet("Shannon"))
+
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+// 
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+// 
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+// 
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+
+function past(h, m, s){
+  if (h >= 0 && h <= 23 && m >= 0 && m <= 59 && s >=0 && s <= 59) { 
+  return (h*3600000) + (m*60000) + (s*1000)
+  } else {
+    return "Check your numbers again"
+  }
+}
+
+//? console.log(past(0, 1, -1))
