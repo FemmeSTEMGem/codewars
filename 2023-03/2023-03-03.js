6f6846669e0e1ecfe28f90de8726b4c0338ed812
femmestemgem <femmestemgem@gmail.com>
2023-03-03 20:34:52 -0500

    Daily fundamentals practice

--- 2023-03/2023-03-03.js
+++ 2023-03/2023-03-03.js
@@ -12,8 +12,34 @@ function getMiddle(s) {
   return s.length % 2 == 0 ? s[s.length/2 - 1] + s[s.length/2] : s[(s.length - 1) / 2]
 }
 
-console.log(getMiddle("testing"))
+//? console.log(getMiddle("testing"))
 
 // function getMiddle(s) {
 //   return s.slice((s.length-1)/2, s.length/2+1);
 // } --> This is another solution that I found. I had no idea that the slice() method uses Math.floor() to round down any float to an integer. Cool!
+
+
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  let array =  []
+  for (let i = 0; i < s.length; i++) {
+    array.push(s[i].toUpperCase())
+    for (let r = 0; r < i; r++) {
+      array.push(s[i].toLowerCase())
+    }
+    array.push("-")
+  }
+  return array.join("").slice(0, -1)
+}
+
+//? console.log(accum("ZpglnRxqenU"))
+
+// Man, this is tricky if you're not aware of .repeat() or the Array object >_>
