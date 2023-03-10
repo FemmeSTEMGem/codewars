bbc7cf12986fbd06cc7b4bab305e321a5bfb8dbe
femmestemgem <femmestemgem@gmail.com>
2023-03-10 14:39:43 -0500

    Daily fundamentals practice

---
+++ 2023-03/2023-03-10.js
@@ -0,0 +1,11 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+function findShort(s){
+  return s.split(" ").map(x => x.length).sort((a, b) => a - b)[0]
+}
+
+//? console.log(findShort("turns out random test cases are easier than writing out basic ones"))
+
