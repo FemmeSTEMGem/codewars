07d00536f7787082d56e1cf6931f25cc23befe9c
femmestemgem <femmestemgem@gmail.com>
2023-05-25 16:09:57 -0400

    Daily fundamentals practice

--- 2023-05/2023-05-25.js
+++ 2023-05/2023-05-25.js
@@ -9,3 +9,24 @@ var countSheep = function (num){
 }
 
 console.log(countSheep(3))
+
+
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+//Original:
+// function greet(name){
+//   return "Hello, " + name + "!";
+//   if(name === "Johnny")
+//     return "Hello, my love!";
+// }
+
+function greet(name){
+  if(name === "Johnny") {
+    return "Hello, my love!"
+  }
+  return "Hello, " + name + "!";
+}
+
+console.log(greet("Shannon"))
