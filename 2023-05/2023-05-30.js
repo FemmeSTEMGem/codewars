83368a8a6ccb08f7cdcddbdd62f19043640d82c8
femmestemgem <femmestemgem@gmail.com>
2023-05-30 20:49:09 -0400

    Daily fundamentals practice

--- 2023-05/2023-05-30.js
+++ 2023-05/2023-05-30.js
@@ -5,20 +5,32 @@
     // "paper", "paper" --> "Draw!"
 
 
-    const rps = (p1, p2) => {
-      if (p1 == p2) {
-        return "Draw!"
-      }
-      if ([p1, p2].includes("scissors") && [p1, p2].includes("paper")) {
-        return p1 == "scissors" ? "Player 1 won!" : "Player 2 won!"
-      }
-      if ([p1, p2].includes("scissors") && [p1, p2].includes("rock")) {
-        return p1 == "scissors" ? "Player 2 won!" : "Player 1 won!"
-      }
-      if ([p1, p2].includes("paper") && [p1, p2].includes("rock")) {
-        return p1 == "paper" ? "Player 1 won!" : "Player 2 won!"
-      }
-    };
+const rps = (p1, p2) => {
+  if (p1 == p2) {
+    return "Draw!"
+  }
+  if ([p1, p2].includes("scissors") && [p1, p2].includes("paper")) {
+    return p1 == "scissors" ? "Player 1 won!" : "Player 2 won!"
+  }
+  if ([p1, p2].includes("scissors") && [p1, p2].includes("rock")) {
+    return p1 == "scissors" ? "Player 2 won!" : "Player 1 won!"
+  }
+  if ([p1, p2].includes("paper") && [p1, p2].includes("rock")) {
+    return p1 == "paper" ? "Player 1 won!" : "Player 2 won!"
+  }
+};
 
-    console.log(rps("scissors", "paper"))
-    //Oops, totally missed the opportunity to make this a lot shorter by creating if/else statements for all the Player 1 wins, then an else that states that Player 2 wins
+console.log(rps("scissors", "paper"))
+//Oops, totally missed the opportunity to make this a lot shorter by creating if/else statements for all the Player 1 wins, then an else that states that Player 2 wins
+
+
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce(
+    (accumulator, currentValue) => accumulator + currentValue)
+}
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))
+//Turns out the initialValue can be left off if you want it to start at 0 - hooray!
