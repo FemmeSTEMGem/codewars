911e7f1daaaddc53b84e1a5feafeb88b80e77d51
femmestemgem <femmestemgem@gmail.com>
2023-06-03 15:48:44 -0400

    Spotty internet is making for repo creation hell - adding project work here temporarily

---
+++ 2023-06/benfordsLawProject.js
@@ -0,0 +1,24 @@
+//Leading number  Probability
+// 1              30.1% 	
+// 2 	            17.6% 	
+// 3 	            12.5% 	
+// 4 	             9.7% 	
+// 5 	             7.9% 	
+// 6 	             6.7% 	
+// 7 	             5.8% 	
+// 8 	             5.1% 	
+// 9 	             4.6%
+
+let array = [345, 45, 3, 87, 246, 5, 457, 120, 436, 135, 436, 235, 87, 3457, 358, 2176, 698]
+
+function lawCalculator(a) {
+  calculatorArray = []
+  return a.sort().map(x => {
+    for (let i = 1; i < 10; count = 0) {
+      let count = 0
+      
+
+    }})
+}
+
+console.log(lawCalculator(array))
