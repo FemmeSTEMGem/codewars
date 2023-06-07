547eb90a97c13dbebbfc78b5437090a6b9d697b2
femmestemgem <femmestemgem@gmail.com>
2023-06-06 20:41:29 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-06.js
+++ 2023-06/2023-06-06.js
@@ -16,3 +16,21 @@ console.log(rentalCarCost(2))
 console.log(rentalCarCost(3))
 console.log(rentalCarCost(7))
 //There's a really beautiful solution where the "d*40" is at the beginning and what's subtracted is the ternary - means you don't have to repeat the d*40 a second time
+
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// 
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// 
+// For example, when the input is green, output should be yellow.
+
+function updateLight(current) {
+  return current == "green" ? "yellow"
+      :  current == "yellow" ? "red"
+      :  "green"
+}
+
+console.log(updateLight("green"))
+console.log(updateLight("yellow"))
+console.log(updateLight("red"))
+//Also saw a clever solution that involved creating an object with keys where the values were the colour they should change to, then calling on the value of the parameter's key
