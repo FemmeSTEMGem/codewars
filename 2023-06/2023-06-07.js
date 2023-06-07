30d18943b8a4269232f62c5bd7f39beff69ecd45
femmestemgem <femmestemgem@gmail.com>
2023-06-07 14:59:08 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-07.js
+++ 2023-06/2023-06-07.js
@@ -18,3 +18,20 @@ console.log(quarterOf(8))
 console.log(quarterOf(11))
 //Other solutions used Math.ceil(month/3) but I don't have the experience to kow if that's just clever or if it's Best Practices
 
+
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+// 
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+// 
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+function setAlarm(employed, vacation){
+  return employed && !vacation ? true : false
+}
+
+console.log(setAlarm(true, true))
+console.log(setAlarm(true, false))
+//After looking at the other answers, I realised I totally missed the opportunity to remove the ternary operation completely and just write "return employed && !vacation"
