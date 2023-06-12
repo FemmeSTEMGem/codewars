f904132366c073e5c8bc2a2d48b3c26f03118e0b
femmestemgem <femmestemgem@gmail.com>
2023-06-12 19:59:22 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-12.js
+++ 2023-06/2023-06-12.js
@@ -11,3 +11,18 @@ function hoopCount (n) {
 
 console.log(hoopCount(3))
 console.log(hoopCount(11))
+
+
+// Your function takes two arguments:
+
+//     current father's age (years)
+//     current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return sonYearsOld * 2 < dadYearsOld ? dadYearsOld - (sonYearsOld * 2) : (sonYearsOld * 2) - dadYearsOld
+}
+
+console.log(twiceAsOld(36, 7))
+//I just learned about Math.abs(), which is the perfect thing to make this a much shorter solution - it finds the difference between two numbers
