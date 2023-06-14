fcab0736e5fe5720bb33f6b37d261c6278c9793f
femmestemgem <femmestemgem@gmail.com>
2023-06-14 15:39:49 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-14.js
+++ 2023-06/2023-06-14.js
@@ -8,6 +8,27 @@ function getAge(inputString){
   return parseInt(inputString[0])
 }
 
-
-console.log(getAge("4 years old"))
+// console.log(getAge("4 years old"))
 //Found out through the other answers that the [0] is redundant and works just fine without it. Cool!
+
+
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+// 
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+// 
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+// 
+    // S is misinterpreted as 5
+    // O is misinterpreted as 0
+    // I is misinterpreted as 1
+// 
+// The test cases contain numbers only by mistake.
+
+function correct(string) {
+	return string.replaceAll(5, "S").replaceAll(1, "I").replaceAll(0, "O")
+}
+
+console.log(correct("BUDAPE5T"))
+console.log(correct("PAR15"))
+console.log(correct("51NGAP0RE"))
+// I wasn't sure, but it's cool to see that .replaceAll() doesn't mind being strung together. Another answer I saw that I think I liked more was turning the corrections into an Object. I like it better because you can more easily see what's being fixed...especially if the number of corrections ever needed to be increased.
