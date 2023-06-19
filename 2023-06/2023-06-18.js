7151e107ca23d94d1ceb31baa27d08e52026c9ca
femmestemgem <femmestemgem@gmail.com>
2023-06-18 23:47:43 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-18.js
+++ 2023-06/2023-06-18.js
@@ -19,3 +19,16 @@ String.prototype.toAlternatingCase = function () {
 
 console.log("Hello".toAlternatingCase())
 //So apparently isLowerCase() is a method...so that was cool to learn. Would have made this function a lot better. Obviously .map() would have been best. Admit I was a bit thrown off by the String.prototype.toAlternatingCase format threw me a little. I think a .map() method with a ternary operator inside with isLowerCase() would have been the perfect balance of readability and efficiency.
+
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(1,2,2)) // 4
+console.log(Kata.getVolumeOfCuboid(6,2,5)) // 60
+// Nothing much to this one, though I did appreciate the few answers I saw that gave gate conditions for various scenarios where things could go wrong...i.e. parameter given is NaN or < 0. Other than that, a very straightforward exercise.
