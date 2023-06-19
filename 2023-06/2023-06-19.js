150f99806396836224ec86d4f525de0e1af22680
femmestemgem <femmestemgem@gmail.com>
2023-06-19 16:40:53 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-19.js
+++ 2023-06/2023-06-19.js
@@ -9,3 +9,14 @@ function divisibleBy(numbers, divisor){
 
 console.log(divisibleBy([1,2,3,4,5,6], 2)) // [2,4,6]
 console.log(divisibleBy([1,2,3,4,5,6], 3)) // [3,6]
+
+
+// Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same a"backwards as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama"
+
+function isPalindrome(x) {
+  return x.toLowerCase() == x.toLowerCase().split('').reverse().join('')
+}
+
+console.log(isPalindrome("Bob")) // true
+console.log(isPalindrome("Madam")) // true
+// I'm glad that I remembered that I don't have to turn true/false outputs into ternary operations. I really liked someone's soluti0on that looked like this: return str === [...str].reverse().join('') - Chef's kiss
