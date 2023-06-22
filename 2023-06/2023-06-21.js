f2930ed60a9bd37e383bd63be20511ce51580733
femmestemgem <femmestemgem@gmail.com>
2023-06-21 22:39:17 -0400

    Daily fundamentals practice

---
+++ 2023-06/2023-06-21.js
@@ -0,0 +1,20 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+// Example: (Input1, Input2 -->Output)
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+//     If either input is an empty string, consider it as zero.
+//     Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+function sumStr(a,b) {
+  if (a == '') {a = '0'}
+  if (b == '') {b = '0'}
+  return (parseInt(a) + parseInt(b)).toString()
+}
+
+console.log(sumStr("4", "")) // "9"
+console.log(sumStr("34", "5")) // "39"
+// I'm a little annoyed that I didn't think of this: return String(Number(a)+Number(b)). Maybe that's what I get for trying to rush this towards the end of the day. This also took longer than it should have because I didn't read the instructions properly. -_- 
