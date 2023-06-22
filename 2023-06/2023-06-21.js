caed9bea4060ee94fcc46cd30d1fcabeacc55ac3
femmestemgem <femmestemgem@gmail.com>
2023-06-21 22:46:30 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-21.js
+++ 2023-06/2023-06-21.js
@@ -15,6 +15,24 @@ function sumStr(a,b) {
   return (parseInt(a) + parseInt(b)).toString()
 }
 
-console.log(sumStr("4", "")) // "9"
-console.log(sumStr("34", "5")) // "39"
-// I'm a little annoyed that I didn't think of this: return String(Number(a)+Number(b)). Maybe that's what I get for trying to rush this towards the end of the day. This also took longer than it should have because I didn't read the instructions properly. -_- 
+// console.log(sumStr("4", "")) // "9"
+// console.log(sumStr("34", "5")) // "39"
+// I'm a little annoyed that I didn't think of this: return String(Number(a)+Number(b)). Maybe that's what I get for trying to rush this towards the end of the day. This also took longer than it should have because I didn't read the instructions properly. -_-
+
+
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+  return Number.isInteger(n) ? n % 2 == 0 : false
+}
+
+console.log(testEven(0))
+console.log(testEven(0.5))
+console.log(testEven(1))
+console.log(testEven(2))
+console.log(testEven(-4))
+// Gosh, I'm so used to avoiding the use of === (strict equals), that I completely missed the fact that it would be so perfectly useful here! --> return n%2===0
