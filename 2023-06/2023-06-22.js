ddf7c9e7083590cb9fccb5816fb637c8a34b3dba
femmestemgem <femmestemgem@gmail.com>
2023-06-22 20:49:14 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-22.js
+++ 2023-06/2023-06-22.js
@@ -31,3 +31,17 @@ console.log(howMuchILoveYou(7)) // "I love you"
 console.log(howMuchILoveYou(3)) // ,"a lot")
 console.log(howMuchILoveYou(6)) //"not at all")
 // A combination of an array and modulo would have been far simpler - I don't know why, but I sometimes resist the use of modulo
+
+
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger. 
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+// Your function will be tested with pre-made examples as well as random ones.
+// If you can, try writing it in one line of code.
+
+function findDifference(a, b) {
+  return Math.abs((a[0]*a[1]*a[2]) - (b[0]*b[1]*b[2]))
+}
+
+console.log(findDifference([3, 2, 5], [1, 4, 4])) // 14);
+console.log(findDifference([9, 7, 2], [5, 2, 2])) // 106);
+// This was just about the best solution. The only one I saw that I liked a little better was one that separated out the parameters --> function findDifference([a, b, c], [d, e, f]) so that you could have a*b*c-d*e*f which was pretty nice looking
