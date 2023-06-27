379ddde792d7a25abf545c08019116e4a5be83dc
femmestemgem <femmestemgem@gmail.com>
2023-06-26 23:05:54 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-26.js
+++ 2023-06/2023-06-26.js
@@ -10,3 +10,15 @@ function createArray(number){
 
 console.log(createArray(1)) // [1]
 // Easy peasy fix. Not much to say re: the other solutions...unless you wanted to redo the function itself.
+
+
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+// The returned value must be a string, and have "***" between each of its letters.
+// You should not remove or add elements from/to the array.
+
+function twoSort(s) {
+  return s.sort().shift().replaceAll('', "***").slice(3, -3)
+}
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])) // 'b***i***t***c***o***i***n' );
+// Whoops, should have just done a split/join. Doing this in a rush was not ideal. >_>
