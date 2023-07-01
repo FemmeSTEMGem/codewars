a9de4d6a62441c543d17f96cff87168adf9b1919
femmestemgem <femmestemgem@gmail.com>
2023-06-30 23:12:31 -0400

    Daily fundamentals practice

--- 2023-06/2023-06-30.js
+++ 2023-06/2023-06-30.js
@@ -48,3 +48,26 @@ function fixTheMeerkat(arr) {
 
 console.log(fixTheMeerkat(["tail", "body", "head"])) // ["head", "body", "tail"]
 // Simple use of the reverse() method
+
+
+
+
+// Create a method to see whether the string is ALL CAPS.
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function() {
+  return this.split('').filter(x => x == x.toUpperCase()).length == this.length
+}
+
+// console.log('c'.isUpperCase()) // false
+// console.log('C'.isUpperCase()) // true
+
+console.log("Hello".split('').filter(x => x == x.toUpperCase()))
+// For some reason I had it in my head that .toUpperCase() could only be used on individual letters rather than a whole string...hence the solution above. Now remembering, I could have just said this == this.toUpperCase()
