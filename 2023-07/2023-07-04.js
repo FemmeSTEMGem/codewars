260e164c4fd81ed328513fb2c3aef1be7078d02b
femmestemgem <femmestemgem@gmail.com>
2023-07-04 17:19:01 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-04.js
+++ 2023-07/2023-07-04.js
@@ -6,3 +6,15 @@ function unusualFive() {
 }
 
 console.log(unusualFive())
+// Mulled over this one for a little longer than I'd like to admit... >_>
+
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x){
+  return typeof x == 'string' ? "Error" : x*50+6
+}
+
+console.log(problem(0))
+console.log(problem("hi"))
+// Short and sweet.
