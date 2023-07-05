5197623b9bda097d970888763d6dab74261ea5d6
femmestemgem <femmestemgem@gmail.com>
2023-07-05 17:19:09 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-05.js
+++ 2023-07/2023-07-05.js
@@ -12,3 +12,20 @@ function findMultiples(integer, limit) {
 
 console.log(findMultiples(2, 6))
 // Looking at the other solutions, this one actually feels like the best. There are some clever people one-lining this one, but it's pretty unreadable.
+
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+    // don't worry about uppercase vowels
+    // y is not considered a vowel for this kata
+
+function shortcut (string) {
+  return string.replace(/[aeiou]/g, '')
+}
+
+console.log(shortcut("codewars"))
+// Needed to go out of my way to learn some regex to do this. Pretty cool!
