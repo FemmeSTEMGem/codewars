1d744f9047d32f36414a2becd144691bc2b65a9d
femmestemgem <femmestemgem@gmail.com>
2023-07-06 20:27:17 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-06.js
+++ 2023-07/2023-07-06.js
@@ -12,3 +12,19 @@ function getRealFloor(n) {
 console.log(getRealFloor(1)) // 0
 console.log(getRealFloor(15)) // 13
 // There are simpler solutions, but I like mine best as a middleground between functionality and readability
+
+
+// Return the Nth Even Number
+// Example(Input --> Output)
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+
+function nthEven(n){
+  return (n - 1) * 2
+}
+
+console.log(nthEven(100))
+// Made my code unecessarily long by using a ternary operator for situations where n = 1, but it'll still work as shown. Simple mistake that I was able to correct once I'd thought about it for a sec. Otherwise, this is about as simple as this solution gets (though n*2-2 is the absolute simplest).
