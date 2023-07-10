9b8097ca046b7f4a746ef19f4a5b985009810651
femmestemgem <femmestemgem@gmail.com>
2023-07-09 21:26:07 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-09.js
+++ 2023-07/2023-07-09.js
@@ -8,3 +8,18 @@ function nameShuffler(str){
 
 console.log(nameShuffler("john McClane"))
 // Confirmed with the other answers that this is straightforward and the best way to do this.
+
+
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+// Hint for R users:
+//     The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+function solution(a, b){
+  return a.length > b.length ? b+a+b : a+b+a
+}
+
+console.log(solution("1111", "22222"))
+// Very satisfying to see that this my solution is the best answer.
