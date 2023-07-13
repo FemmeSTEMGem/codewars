140b0a3d35f0933542ddbf3c88ec21da97ce945e
femmestemgem <femmestemgem@gmail.com>
2023-07-12 20:03:13 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-12.js
+++ 2023-07/2023-07-12.js
@@ -17,3 +17,21 @@ function pipeFix(numbers){
 
 console.log(pipeFix([1,3,5,6,7,8]))
 // I thought for sure there must be a better way of doing this, but the other responses seem to show that this is the best solution. For clarity, I would probably save the Math.min/max(...numbers) as variables just to tidy things up a bit.
+
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+
+function remove (string) {
+  return string.split("")[string.length - 1] == "!" ? string.split("").slice(0, -1).join("") : string
+}
+
+console.log(remove("!!Hi!!"))
+// Oof, in one fell swoop I was reminded that .slice() can be used on a String as well as an Array...AND about the existence of .endsWith():
+  // return string.endsWith('!') ? string.slice(0, -1) : string
