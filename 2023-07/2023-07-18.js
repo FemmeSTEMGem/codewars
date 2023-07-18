cf0a82f97e3835cadb5f86a8fb2848c96d88ed41
femmestemgem <femmestemgem@gmail.com>
2023-07-18 18:37:05 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-18.js
+++ 2023-07/2023-07-18.js
@@ -10,3 +10,19 @@ function capitalizeWord(word) {
 
 console.log(capitalizeWord("glasswear"))
 // Honestly, among the best solutions. Only other thing I saw that's comparable was using .toUpperCase() combined with .slice()
+
+
+// The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+function mouthSize(animal) {
+  return animal.toLowerCase() == "alligator" ? "small" : "wide"
+}
+
+console.log(mouthSize("Alligator"))
+// Nice and simple ternary operation.
