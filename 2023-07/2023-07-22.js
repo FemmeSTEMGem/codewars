3f794ba2368d9e4d5b3cfbcf528a64362bf6dd11
femmestemgem <femmestemgem@gmail.com>
2023-07-22 13:59:51 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-22.js
+++ 2023-07/2023-07-22.js
@@ -11,5 +11,18 @@ function howManyDalmatians(num) {
   : "Woah that's a lot of dogs!"
 }
 
-console.log(howManyDalmations(100))
+console.log(howManyDalmatians(100))
 // Very happy with my solution (some people chose to keep the format the same; I prefered to change it). Sneaky trick introducing spelling errors as well.
+
+
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter){
+  let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  return `Position of alphabet: ${alphabet.indexOf(letter) +1}`
+}
+
+console.log(position("a"))
+// Welp, today I learned about .charCodeAt() which, when combined with "- 96" would give me the right answer as opposed to having to type out all the nonsense above. I was surprised... was expecting to see some answers using regex, but maybe there isn't a way to do that that's more efficient than not...
