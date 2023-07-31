83ed4156971534d48737a5ca620c4f02e966dde5
femmestemgem <femmestemgem@gmail.com>
2023-07-30 20:19:59 -0400

    Daily fundamentals practice

--- 2023-07/2023-07-30.js
+++ 2023-07/2023-07-30.js
@@ -7,3 +7,14 @@ function hello(name) {
 
 console.log(hello('johN'))
 // Despite how long-winded it is to make the first letter uppercase and the rest lowercase, this is the best solution.
+
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
+
+console.log(binToDec("1001001"))
+
+// Absolutely had to look this up online as far as translating binary into regular numbers. I wasn't aware that parseInt could take a second argument called "radix". I'm still not super clear on what that second argument is doing, but maybe I don't super need to know for now. ^_^
