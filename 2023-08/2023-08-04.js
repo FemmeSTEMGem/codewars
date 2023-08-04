514e6b0cb62fbb82e2519536644fe8343f3b37bc
femmestemgem <femmestemgem@gmail.com>
2023-08-04 13:24:54 -0400

    Daily fundamentals practice

--- 2023-08/2023-08-04.js
+++ 2023-08/2023-08-04.js
@@ -25,3 +25,33 @@ function seatsInTheater(nCols, nRows, col, row) {
 
 console.log(seatsInTheater(16,11,5,3))
 // The only difference between my solution and the others is that some people did the second half of the calculation as (nCols - col + 1), which is maybe a bit cleaner. ^_^
+
+
+// I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+
+function getLength(arr){
+  return arr.length
+}
+function getFirst(arr){
+  return arr.shift()
+}
+function getLast(arr){
+  return arr.pop()
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  return arr
+}
+function popElement(arr){
+  arr.pop()
+  return arr
+}
+
+console.log(popElement([1,2,3]))
+// Honestly, kind've just a nice reminder on some rarely used (at least for me) Array methods (shift and pop).
