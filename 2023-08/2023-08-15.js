a24cf443e0ff950beb00be001600c5eb914b3352
femmestemgem <femmestemgem@gmail.com>
2023-08-15 23:59:08 -0400

    Daily fundamentals practice

--- 2023-08/2023-08-15.js
+++ 2023-08/2023-08-15.js
@@ -11,3 +11,13 @@ function formatMoney(amount){
 
 console.log(formatMoney(3.55))
 // I had to look up .toFixed() but I knew something had to exist that did what it does. Easy peasy after that. :)
+
+
+// Find the mean (average) of a list of numbers in an array.
+
+function findAverage(nums) {
+  return nums.reduce((a, b) => a + b, 0)/nums.length
+}
+
+console.log(findAverage([1, 3, 5, 7]))
+// The .reduce() function likely has a lot of uses; it just happens to be handy for adding all elements in an array together. I'd like to know the other common use cases at some point because it seems potentially powerful but I don't know how yet.
