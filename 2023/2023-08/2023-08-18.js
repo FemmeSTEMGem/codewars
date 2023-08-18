3703b51bdfe5b7e7a51938d72c5872dcec28dfd8
femmestemgem <femmestemgem@gmail.com>
2023-08-18 12:05:05 -0400

    Daily fundamentals practice

--- 2023/2023-08/2023-08-18.js
+++ 2023/2023-08/2023-08-18.js
@@ -9,4 +9,19 @@
 function sayHello( name, city, state ) {
   return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
 }
-//So simple I didn't even bother testing it before submitting.
+// So simple I didn't even bother testing it before submitting.
+
+
+// Inspired by the development team at Vooza, write the function that
+//     accepts the name of a programmer, and
+//     returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+// Note: your function should have a default parameter.
+// For example(Input --> Output):
+// "anyone else" --> 0
+// "Zach" --> 18
+
+function howManyLightsabersDoYouOwn(name) {
+  return name == "Zach" ? 18 : 0
+}
+// Also didn't bother testing, but the prompt made me laugh.
