d563f650941a75295c44a35487e20ff70f90a52c
femmestemgem <femmestemgem@gmail.com>
2023-08-23 14:55:23 -0400

    Daily fundamentals practice

--- 2023/2023-08/2023-08-23.js
+++ 2023/2023-08/2023-08-23.js
@@ -9,3 +9,13 @@ function spEng(sentence){
 
 console.log(spEng("abcnEnglishsef"))
 // Easy enough, though for some reason I had it in my head that .includes() was exclusive to arrays, but I must have been thinking about a different method.
+
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+function sumOfDifferences(arr) {
+  return arr.sort((a, b) => b - a).map((x, y) => x - arr[y + 1] || 0).reduce((c, d) => c + d, 0)
+}
+
+console.log(sumOfDifferences([2, 1, 10]))
+// I'll admit, I absolutely had to go to the solutions for this one. A bit embarassing. Definitely one of those "more of a math problem than a coding problem" things. I had the right shape of the solution; just didn't quite get the right things in place for .map() and .reduce(). The better solution was just taking the largest number in the array and subtracting the smallest number in the array. Must faster and more efficient code. Ugh.
