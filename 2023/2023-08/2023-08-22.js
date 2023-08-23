3bdca5eb116a28defd7069d9cde2a8d0ff06ae96
femmestemgem <femmestemgem@gmail.com>
2023-08-22 20:04:28 -0400

    Daily fundamentals practice

--- 2023/2023-08/2023-08-22.js
+++ 2023/2023-08/2023-08-22.js
@@ -5,4 +5,14 @@ function getSize(width, height, depth) {
 }
 
 console.log(getSize(4, 2, 6))
-// More of a math problem than a coding problem; simple enought.
+// More of a math problem than a coding problem; simple enough.
+
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+function check(a,x){
+  return a.includes(x)
+}
+
+// Didn't even bother to test, but a really good kata if people don't know about includes()...time to find out about it!
