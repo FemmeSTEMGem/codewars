b941dda3d83c0ec435ac05fac904f0ac7e7e1d01
femmestemgem <femmestemgem@gmail.com>
2023-08-29 15:21:07 -0400

    Daily fundamentals practice

--- 2023/2023-08/2023-08-29.js
+++ 2023/2023-08/2023-08-29.js
@@ -18,3 +18,25 @@ function sumMul(n,m){
 
 console.log(sumMul(0, 0))
 // Fairly tidy. I think I like my solution best. The shorter solutions are a little too muddled for a human to read easily I think.
+
+
+// Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+//     left - The current floor of the left elevator
+//     right - The current floor of the right elevator
+//     call - The floor that called an elevator
+// It should return the name of the elevator closest to the called floor ("left"/"right").
+// In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+// You can assume that the inputs will always be valid integers between 0-2.
+// Examples:
+// elevator(0, 1, 0); // => "left"
+// elevator(0, 1, 1); // => "right"
+// elevator(0, 1, 2); // => "right"
+// elevator(0, 0, 0); // => "right"
+// elevator(0, 2, 1); // => "right"
+
+function elevator(left, right, call){
+  return Math.abs(left - call) < Math.abs(right - call) ? "left" : "right"
+}
+
+console.log(elevator(0, 1, 1))
+// Tidy and efficient. Oh yeah. :)
