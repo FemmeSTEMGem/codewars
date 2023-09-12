2fa28709565529c50211c0494ee70bfa3c532708
femmestemgem <femmestemgem@gmail.com>
2023-09-12 14:41:03 -0400

    Daily fundamentals practice

--- 2023/2023-09/2023-09-12.js
+++ 2023/2023-09/2023-09-12.js
@@ -9,3 +9,22 @@ function squareArea(a){
 
 console.log((squareArea(14.05)))
 // Annoyingly, I wasn't able to find any good resources online for the geometry math, so I ended up having to look at the answers. BUT, I learned some new stuff about .toFixed() that somehow never came up before. I didn't know that it turned numbers into strings and that this can be fixed by adding "+" in front of the number you're .toFixed()'ing to. ^_^
+
+
+// Messi goal scoring statistics:
+// La Liga 	43
+// Champions League 	10
+// Copa del Rey 	5
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+
+let laLigaGoals = 43
+let championsLeagueGoals = 10
+let copaDelReyGoals = 5
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+// Too easy for this kata level, but if I were doing this for realzies I'd probably store these in an object.
