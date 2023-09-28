540cd00c13275f9f8e4c1ee2b7d9cb2fd1dabd5b
femmestemgem <femmestemgem@gmail.com>
2023-09-28 17:58:01 -0400

    Daily fundamentals practice

--- 2023/2023-09/2023-09-28.js
+++ 2023/2023-09/2023-09-28.js
@@ -20,3 +20,17 @@ function sameCase(a, b){
 // Of the solutions in CodeWars, I'm really liking mine the most. Though I did learn an interesting thing (more clever than useful in this context):
   // sameCase = (a,b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1
 // I think I forgot that the Number value of true == 1. Clever application, though unclear in a real-world scenario.
+
+
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+    // number of pillars (≥ 1);
+    // distance between pillars (10 - 30 meters);
+    // width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+function pillars(numPill, dist, width) {
+  return numPill >= 2 ? ((dist * (numPill - 1)*100)) + (width * (numPill - 2)) : 0
+}
+
+// console.log(pillars(11, 15, 30)) // 15270);
+// Hilariously, the only reason that this one was so difficult is that I missed that we were dealing with multiple units of measure. Nice to be humbled by an 8 kyu problem in CodeWars, lol.
