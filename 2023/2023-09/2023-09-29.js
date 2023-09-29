124fa3dc82be56f5b4fd0a9d4898ee5a617864a0
femmestemgem <femmestemgem@gmail.com>
2023-09-29 19:30:18 -0400

    Daily fundamentals practice

--- 2023/2023-09/2023-09-29.js
+++ 2023/2023-09/2023-09-29.js
@@ -15,3 +15,25 @@ function sixToast(num) {
 
 console.log(sixToast(6))
 // One result that I think is better is Math.abs(6 - n)...also return num >= 6 ? num - 6 : num. Both much better. Gotta keep Math.abs() in mind more often.
+
+
+// An AI has infected a text with a character!!
+// This text is now fully mutated to this character.
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+// Note: The character is a string of length 1 or an empty string.
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+// function contamination(text, char){
+//   return !text || !char ? "" : char.repeat(text.length)
+// }
+
+function contamination(text, char) {
+  return text.replace(/./g, char);
+}
+
+console.log(contamination("", "z"))
+// Better solutions include one without the gate condition that checks if text or char is true...because it works fine without it. However, as one commenter pointed out, this wouldn't be a good solution for best practices because the intent to return an empty string if text or char is empty wouldn't be clear. Instead, the best solution I think uses regex: return text.replace(/./g, char), but mine is a pretty close second, imo.
