e42d0281dcac9fde0c036afb6439e47684db742d
femmestemgem <femmestemgem@gmail.com>
2023-09-30 13:13:36 -0400

    Daily fundamentals practice

--- 2023/2023-09/2023-09-30.rb
+++ 2023/2023-09/2023-09-30.rb
@@ -15,3 +15,18 @@ end
 
 print summation(2)
 # Welp, looks like I completely forgot about the sum method in Ruby. Interestingly, I tried to use "sum" in a different way, just as a stab in the dark because I couldn't remember how to add up a bunch of numbers in Ruby. It's good to be getting back into this, though. The best answer was: (1..num).sum
+
+
+# Write a function that removes the spaces from the string, then return the resultant string.
+# Examples:
+# Input -> Output
+# "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+# "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+# "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+def no_space(x)
+  return x.gsub(" ", "")
+end
+
+print no_space('8 j 8   mBliB8g  imjB8B8  jl  B')
+# Slightly more efficient, the best solution I saw was actually using x.delete(" ")...but I learned about two different methods today, so I'm happy.
