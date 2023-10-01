a00e6e437789f845fc6231b07d8511f4e0257aca
femmestemgem <femmestemgem@gmail.com>
2023-10-01 02:28:46 -0400

    Daily fundamentals practice

--- 2023/2023-10/2023-10-01.sql
+++ 2023/2023-10/2023-10-01.sql
@@ -16,3 +16,17 @@ SELECT
 FROM
   kata
 -- Struggled with this one a bit longer than I should because I misread the instruction. Note to self, "slow down, don't rush!".
+
+
+-- In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+-- In SQL, you will be given a table moves with columns position and roll. Return a table which uses the current position of the hero and the roll (1-6) and returns the new position in a column res.
+-- Example:
+-- move(3, 6) should equal 15
+
+--# write your statement here: you will be given a table 'moves' with columns 'position' and 'roll'. return a table with a column 'res'. #--
+
+SELECT
+  position + (roll * 2) AS res
+FROM
+  moves
+-- This is maybe a little silly, but this is the first one I got right without having to look things up since I started relearning SQL and I'm so happy about it.
