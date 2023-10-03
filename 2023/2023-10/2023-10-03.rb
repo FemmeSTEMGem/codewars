ae21ff00d5c91813e5c92d5ba21ae9b23422951e
femmestemgem <femmestemgem@gmail.com>
2023-10-03 15:36:32 -0400

    Daily fundamentals practice

--- 2023/2023-10/2023-10-03.rb
+++ 2023/2023-10/2023-10-03.rb
@@ -12,3 +12,18 @@
   
   # print string_to_number("7")
   # Simple enough. Nice to get back  into the swing of Ruby bit by bit. ^_^ Was also reminded that Integer(s) would have also sufficed.
+
+
+# Can you find the needle in the haystack?
+# Write a function findNeedle() that takes an array full of junk but containing one "needle"
+# After your function finds the needle it should return a message (as a string) that says:
+# "found the needle at position " plus the index it found the needle, so:
+# Example(Input --> Output)
+# ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+def find_needle(haystack)
+  return "found the needle at position #{haystack.index('needle')}"
+end
+
+print find_needle(['3', '123124234', nil, 'needle', 'world', 'hay', 2, '3', true, false]) # 'found the needle at position 3'
+# Mine is the better solution. Only issue I had was remembering how to interpolate in Ruby. :)
