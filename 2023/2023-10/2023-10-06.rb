9d9df01f45308fd4bea7b0273421cd6d46d8546d
femmestemgem <femmestemgem@gmail.com>
2023-10-06 12:40:22 -0400

    Daily fundamentals practice

---
+++ 2023/2023-10/2023-10-06.rb
@@ -0,0 +1,8 @@
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+def greet
+  return "hello " + "world" + "!"
+end
+
+print greet
+# Look, these are cute exercises and all, but I'm doing them to both practice what I know and learn things. Kata like these aren't very useful to me. Just wanna complete theis one and move on, lol.
