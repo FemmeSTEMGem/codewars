cc5b58ac65dec281bc54b2aedf74a3c869ea9cb1
femmestemgem <femmestemgem@gmail.com>
2023-10-10 14:36:42 -0400

    Daily fundamentals practice

--- 2023/2023-10/2023-10-10.rb
+++ 2023/2023-10/2023-10-10.rb
@@ -14,3 +14,14 @@ end
 
 print bmi(50, 1.80)
 # Other strategies used case/when instead, but I think mine is tidier. :)
+
+
+# A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+# Return true if yes, false otherwise :)
+
+def hero(bullets, dragons)
+  return bullets >= dragons*2
+end
+
+print hero(7, 4)
+# Other people had similarly short answers, but I like mine the best because I find it more clear.
