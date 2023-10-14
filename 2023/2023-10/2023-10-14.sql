46678952c32fc2addf9bfa087bdc7c33113e4602
femmestemgem <femmestemgem@gmail.com>
2023-10-14 11:15:01 -0400

    Daily fundamentals practice

--- 2023/2023-10/2023-10-14.sql
+++ 2023/2023-10/2023-10-14.sql
@@ -10,3 +10,14 @@ SELECT
 FROM
   decimals
 -- Easy peasy. I feel like I'm getting my SQL sea legs again.
+
+
+-- Using only SQL, write a query that returns all rows in the custid, custname, and custstate columns from the customers table.
+
+SELECT
+  custid,
+  custname,
+  custstate
+FROM
+  customers
+-- Another easy peasy one. :)
