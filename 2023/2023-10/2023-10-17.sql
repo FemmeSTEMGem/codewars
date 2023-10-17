44a35bd6ba440cf39926b2e99364b57530b3cd30
femmestemgem <femmestemgem@gmail.com>
2023-10-17 17:07:15 -0400

    Daily fundamentals practice

--- 2023/2023-10/2023-10-17.sql
+++ 2023/2023-10/2023-10-17.sql
@@ -15,3 +15,12 @@ SELECT
 FROM
   numbers
 -- Got tripped up by a few things on this one. First, I tried to look up "if" statements for SQL. I found the syntax but, it turns out that CodeWars specifically uses PostgreSQL which uses a different syntax for "if" statements. I ended up using "case" instead (I took a peek at the answers just to see what to use), but put together the SQL statement on my own. I also learned that assigning the name of the column can be done at the end of the case statement, which is nice and cuts down on repetition. Otherwise, I had the right shape of the solution which I'm happy about.
+
+
+-- This code does not execute properly. Try to figure out why.
+
+SELECT
+  (price * amount) AS total
+FROM
+  items
+-- Something interesting I just learned from this one is that the brackets don't matter at all. The solution works regardless. However, I noticed that more people used the brackets rather than leaving them out, so maybe it's a stylistic thing. For me, I think it would be easier to read a more complex query if you put things like this into brackets.
