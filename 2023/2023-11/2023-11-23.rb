6cae654bb424b2f85951e69446aa83b92e33eed2
femmestemgem <femmestemgem@gmail.com>
2023-11-23 23:38:26 -0500

    Daily fundamentals practice

---
+++ 2023/2023-11/2023-11-23.rb
@@ -0,0 +1,11 @@
+# Complete the solution so that it reverses the string passed into it.
+# 
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+def solution(str)
+  str.reverse
+end
+
+print solution("hello")
+# Easy peasy, but I still learned something new! I don't think I knew that I could call a function without attaching "()" to the end. Or maybe I'm just having holdover habits from JavaScript. ^_^
