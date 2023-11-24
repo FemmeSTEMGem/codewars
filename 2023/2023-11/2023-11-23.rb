333113d38fa991e4a90f697629aa2d30799a2fc0
femmestemgem <femmestemgem@gmail.com>
2023-11-23 23:42:37 -0500

    Daily fundamentals practice

--- 2023/2023-11/2023-11-23.rb
+++ 2023/2023-11/2023-11-23.rb
@@ -9,3 +9,13 @@ end
 
 print solution("hello")
 # Easy peasy, but I still learned something new! I don't think I knew that I could call a function without attaching "()" to the end. Or maybe I'm just having holdover habits from JavaScript. ^_^
+
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word bool
+  bool ? "Yes" : "No"
+end
+
+print bool_to_word(true)
+# Gahhh, I love Ruby so much! So simple and clean and beautiful. Also, I completely forgot that you can declare a parameter without needing to use brackets in Ruby. *chefs kiss*
