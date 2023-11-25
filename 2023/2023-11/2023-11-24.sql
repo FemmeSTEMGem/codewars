f26cd2672b1f9f46f19c810bfe67b7527611f85f
femmestemgem <femmestemgem@gmail.com>
2023-11-24 20:18:22 -0500

    Daily fundamentals practice

--- 2023/2023-11/2023-11-24.sql
+++ 2023/2023-11/2023-11-24.sql
@@ -10,3 +10,14 @@ SELECT
 FROM
   booltoword
 -- I definitely haven't used CASE very much in SQL and I'm honestly glad that I'm getting a bit more practice with it. It'll obviously be incredibly useful.
+
+
+-- It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+-- # write your SQL statement here: you are given a table 'removechar' with column 's', return a table with column 's' and your result in a column named 'res'.
+
+SELECT
+  s,
+  SUBSTRING(s, 2, LENGTH(s) - 2) AS res
+FROM
+  removechar
+-- Not too shabby at all once I found the SUBSTRING method! ^_^
