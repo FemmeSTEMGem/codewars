e65f6b68c7a6d6a99cb49a60373c9e737d3bc04a
femmestemgem <femmestemgem@gmail.com>
2023-11-27 18:48:56 -0500

    Daily fundamentals practice

--- 2023/2023-11/2023-11-27.rb
+++ 2023/2023-11/2023-11-27.rb
@@ -9,3 +9,18 @@ def number_to_string(num)
   num.to_s
 end
 # Easy peasy. Couldn't remember the exact name of the method, so I had to Google it. But the naming convention was correct.
+
+
+# Very simple, given an integer or a floating-point number, find its opposite.
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+def opposite x
+  -x
+end
+
+print opposite(-2)
+print opposite(2)
+# Originally solved this as a ternary operation, then realised through trial and error that I could simplify even further. :)
