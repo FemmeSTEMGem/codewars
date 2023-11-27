b19adfb1e138294c679cc233d8895beb83098a78
femmestemgem <femmestemgem@gmail.com>
2023-11-27 18:42:33 -0500

    Daily fundamentals practice

---
+++ 2023/2023-11/2023-11-27.rb
@@ -0,0 +1,11 @@
+# We need a function that can transform a number (integer) into a string.
+# What ways of achieving this do you know?
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+def number_to_string(num)
+  num.to_s
+end
+# Easy peasy. Couldn't remember the exact name of the method, so I had to Google it. But the naming convention was correct.
