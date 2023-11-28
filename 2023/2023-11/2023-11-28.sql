69ed17122842cb923595830c13359c143f2eb151
femmestemgem <femmestemgem@gmail.com>
2023-11-28 17:48:06 -0500

    Daily fundamentals practice

--- 2023/2023-11/2023-11-28.sql
+++ 2023/2023-11/2023-11-28.sql
@@ -10,3 +10,18 @@ SELECT
 FROM
   opposite
 -- I took a complete stab in the dark hoping that numbers would work this way in SQL and they do! Hooray!
+
+
+-- Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+-- Examples (input -> output)
+-- 6, "I"     -> "IIIIII"
+-- 5, "Hello" -> "HelloHelloHelloHelloHello"
+-- --# write your SQL statement here: you are given a table 'repeatstr' with columns 'n' and 's', return a table with all columns and your result in a column named 'res'.
+
+SELECT
+  n,
+  s,
+  REPEAT(s, n) AS res
+FROM
+  repeatstr
+-- Done perfectly. Hit a minor hitch when the results were returned in a different order than the tests were expecting. But I figured it out right away (despite the order not being part of the instructions) and it was fixed on the first try.
