033f790243956e0fa1504444e1783468c53dfd28
femmestemgem <femmestemgem@gmail.com>
2023-12-07 15:04:37 -0500

    Daily fundamentals practice

--- 2023/2023-12/2023-12-07.rb
+++ 2023/2023-12/2023-12-07.rb
@@ -7,3 +7,16 @@ end
 
 print square_sum([1, 2])
 # It's interesting to me that one of the top-voted solutions is the same as mine, but with .reduce(:+) instead of simply .sum. That seems strange to me. The top comment for it, though says "I take it that this used to be best practice prior to the inclusion of .sum in Ruby ~2.4. Or is reduce/inject still preferred?"...so this solution may just be out of date. Interesting that there was no sum in an earlier version of Ruby!
+
+
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str (n, s)
+  s*n
+end
+
+print repeat_str(3, "*")
+# This one was a little bit funny. I was thinking of the JavaScript method .repeat() and trying to find something similar in Ruby. I tried to make .times() work for a bit until I realised that there was a far simpler way to solve this problem, lol. ^_^
