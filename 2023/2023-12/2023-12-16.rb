adfa77407697eed20d896f299efe2f833d33b3ae
femmestemgem <femmestemgem@gmail.com>
2023-12-16 23:23:08 -0500

    Daily fundamentals practice

--- 2023/2023-12/2023-12-16.rb
+++ 2023/2023-12/2023-12-16.rb
@@ -14,3 +14,18 @@ end
 
 # print century(1705)
 # This one actually bamboozled me for a bit because I forgot the rule in Ruby where, when you perform arithmetic operations in Ruby with two integers, the result will automatically be an integer. If you want your result to include any decimals, at least one of the numbers needs to include a decimal place. So, in my solution above, I couldn't figure out why (year / 100).ceil wasn't working. Now I know! Also, it's very simple math, but I remember doing this problem in another language at some point and it just wasn't clicking for me. Just goes to show how your brain can have dumb days with the simplest things sometimes. ^_^
+
+
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+# It should look like this:
+# Sam Harris => S.H
+# patrick feeney => P.F
+
+def abbrev_name(name)
+  "#{name.split[0][0]}.#{name.split[1][0]}".upcase()
+end
+
+print abbrev_name("patrick feeney")
+# Annoyingly, the solution that was the same as mine was voted more as "Clever" than "Best Practices". And I don't agree with the top result for "Best Practices"...it's too messy to me. The other result I liked, though I don't understand it was:
+# name.upcase.split.map(&:chr).join"."
