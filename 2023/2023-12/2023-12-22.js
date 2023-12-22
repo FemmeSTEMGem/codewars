b6673339d83e457a7c31aa5ccc66a378c7e3ae9c
femmestemgem <femmestemgem@gmail.com>
2023-12-22 00:27:59 -0500

    Daily fundamentals practice

---
+++ 2023/2023-12/2023-12-22.js
@@ -0,0 +1,6 @@
+// Return the type of the sum of the two arguments
+
+function typeOfSum(a, b) {
+  return typeof (a+b)
+}
+// Made a silly little mistake in forgetting to wrap the a+b in brackets, but just fine otherwise. ^_^
