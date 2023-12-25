2901099e4304253cf65448f12cc5e1d23cc75b68
femmestemgem <femmestemgem@gmail.com>
2023-12-25 16:34:21 -0500

    Daily fundamentals practice

--- 2023/2023-12/2023-12-25.js
+++ 2023/2023-12/2023-12-25.js
@@ -7,3 +7,16 @@ function ifChuckSaysSo() {
   return !true
 }
 // Easy, but a cute exercise so I don't mind. ^_^
+
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
+
+function multiply(number){
+  return number * 5**`${Math.abs(number)}`.length
+}
+// My solution was about right. The only thing that might make it better would be the use of .toString() instead of using interpolation. Still, glad I got one of the better solutions.
