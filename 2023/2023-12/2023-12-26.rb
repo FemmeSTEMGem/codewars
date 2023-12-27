f3381ec8931e11f91a9f36ad39d8b8d509124a01
femmestemgem <femmestemgem@gmail.com>
2023-12-26 23:23:29 -0500

    Daily fundamentals practice

---
+++ 2023/2023-12/2023-12-26.rb
@@ -0,0 +1,8 @@
+# Given an array of integers, return a new array with each value doubled.
+# For example:
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(x)
+  x.map {|x| x*2}
+end
+# Just a minor syntax error after just having come off of JavaScript - I used brackets instead of curly braces. Other than that, easy peasy.
