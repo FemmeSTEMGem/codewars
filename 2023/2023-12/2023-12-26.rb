f1877f280bfd1bf1c669359b0bf4ce6d43f4c3e9
femmestemgem <femmestemgem@gmail.com>
2023-12-26 23:23:45 -0500

    Daily fundamentals practice

--- 2023/2023-12/2023-12-26.rb
+++ 2023/2023-12/2023-12-26.rb
@@ -6,3 +6,17 @@ def maps(x)
   x.map {|x| x*2}
 end
 # Just a minor syntax error after just having come off of JavaScript - I used brackets instead of curly braces. Other than that, easy peasy.
+
+
+# Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
+
+def paperwork(n, m)
+  n < 0 || m < 0 ? n*m : 0
+end
+
+print paperwork(5, 5)
+# Made an uncharacteristically silly mistake with this one by putting the return statements on the wrong side of the colon. I blame the cold medication, lol.
