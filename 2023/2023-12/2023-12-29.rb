da6cb5b2d63efa348355a7de5c33f5250b78baee
femmestemgem <femmestemgem@gmail.com>
2023-12-29 18:52:05 -0500

    Daily fundamentals practice

---
+++ 2023/2023-12/2023-12-29.rb
@@ -0,0 +1,7 @@
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+def boolean_to_string(b)
+  return b.to_s
+end
+# I remembered that there was a method for turning things into strings, I just couldn't remeber what it was. I guessed and got it right! ^_^
