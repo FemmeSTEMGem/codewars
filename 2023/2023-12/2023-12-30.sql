c75b6c0a78ba19bc01a4e51f20eecf0117f2ab36
femmestemgem <femmestemgem@gmail.com>
2023-12-30 16:00:41 -0500

    Daily fundamentals practice

--- 2023/2023-12/2023-12-30.sql
+++ 2023/2023-12/2023-12-30.sql
@@ -13,3 +13,27 @@ SELECT
 FROM
   goals
 -- Simple addition, but it never hurts to practice the basics when you're getting back into things. :)
+
+
+-- Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+-- In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+-- SELECT /* your query - you are given columns a, b, and c */ AS res FROM expression_matter;
+-- Notes
+  -- The numbers are always positive.
+  -- The numbers are in the range (1  ≤  a, b, c  ≤  10).
+  -- You can use the same operation more than once.
+  -- It's not necessary to place all the signs and brackets.
+  -- Repetition in numbers may occur .
+  -- You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+
+SELECT
+  GREATEST((a * b) + c,
+  a * (b + c),
+  a * b * c,
+  a + b + c,
+  (a + b) * c,
+  a + (b * c)
+  ) AS res
+FROM
+  expression_matter
+-- I spent way too long trying to figure out how to get arrays to work in SQL until I found out about GREATEST which is just so much faster for this problem. Also ended up with more variations that some of the solutions, so clearly some of them are redundant.
