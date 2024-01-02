da9dc145ce5fba7d7210b24e84390edfb70ae836
femmestemgem <femmestemgem@gmail.com>
2024-01-02 16:08:56 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-02.rb
+++ 2024/2024-01/2024-01-02.rb
@@ -11,3 +11,19 @@ end
 
 print better_than_average([100, 40, 34, 57, 29, 72, 57, 88], 75)
 # There's another solution that's exactly the same as mine, but uses arr.inject(:+) instead of arr.sum. Turns out that's an older Ruby version way of adding elements in an array. Using sum was introduced in Ruby 2.4.0. Thanks, ChatGPT!
+
+
+# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+# Assumptions
+#     You can assume that you are only given numbers.
+#     You cannot assume the size of the array.
+#     You can assume that you do get an array and if the array is empty, return 0.
+
+def sum(numbers)
+  numbers.sum
+end
+
+def sum(numbers)
+  numbers.inject(0, :+)
+end
+# I solved it two ways for funzies, just to play with .inject. Like a leanred above, it needs the accumulator 0 so that it will still return a number if the array is empty. Good to know for old code! (though obviously you'd want to update it if you come across .inject in the wild)
