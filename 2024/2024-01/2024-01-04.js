6c7af9568a2e41c68e04bc570f5f5621eb5fa224
femmestemgem <femmestemgem@gmail.com>
2024-01-04 15:47:27 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-04.js
+++ 2024/2024-01/2024-01-04.js
@@ -11,3 +11,17 @@ function quote(fighter) {
   return fighter.toLowerCase() == "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
 }
 // Easy peasy, though if I were doing this in the real world, I'd account for an invalid parameter. Also, kinda funny because I was coming from SQL land, I did a single = instead of ==...but I was able to figure out the error quickly at least. ^_^
+
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'}
+  return results
+}
+
+console.log(mystery())
+//  This problem was actually really interesting, despite being really straightforward. The original funtion looked exactly the same, except that "results" was on its own one line down. The first thing I noticed was that "results" was greyed out...so I brought it up next to "return" and it lit up again. Easy peasy.
+// It has always been my understanding that JavaScript doesn't usually care about whitespace or line breaks, so I wanted to know why the code behaved this way. ChatGPT to the rescue!:
+// "Due to automatic semicolon insertion (ASI) in JavaScript, a semicolon is inserted after the return statement, effectively causing the function to return undefined."
