b270f9228ce55d83580d5f7c74a8a155ceb60b56
femmestemgem <femmestemgem@gmail.com>
2024-01-07 21:30:08 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-07.rb
+++ 2024/2024-01/2024-01-07.rb
@@ -11,3 +11,12 @@ end
 
 # print invert([1,2,-3,4,5])
 # This is far longer than it needs to be; {|x| -x} would have sufficed. I think if I'd taken a second to refactor, I might have thought of it as more of a math question than a sentence. ^_^
+
+
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+def simple_multiplication number 
+  number.even? ? number * 8 : number * 9
+end
+
+# I was stuck on this one for such a dumb reason. I had worn a groove in my brain with the previous exercise that I wrote number.positive? instead of number.even?. What a bizarre mistake, lol.
