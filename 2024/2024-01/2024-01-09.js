697a85e58d08ae0a6e9484f6af2e464588c9133a
femmestemgem <femmestemgem@gmail.com>
2024-01-09 15:33:40 -0500

    Daily fundamentals practice

---
+++ 2024/2024-01/2024-01-09.js
@@ -0,0 +1,12 @@
+// Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  get info() {
+    return `${this.name}s age is ${this.age}`
+  }
+}
+// Definitely had to refresh myself on Classes in order to do this one.
