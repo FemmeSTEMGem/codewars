0d69dae5571c3f561e7c634d6f5051412190ceb2
femmestemgem <femmestemgem@gmail.com>
2024-01-09 15:34:07 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-09.js
+++ 2024/2024-01/2024-01-09.js
@@ -10,3 +10,24 @@ class Person {
   }
 }
 // Definitely had to refresh myself on Classes in order to do this one.
+
+
+// In this Kata you should fix/create a program that returns the following values:
+//     false/False if either a or b (or both) are not numbers
+//     a % b plus b % a if both arguments are numbers
+// You may assume the following:
+//     If a and b are both numbers, neither of a or b will be 0.
+// Extension: Once you have fixed all the syntax errors present in the code (basic requirement), you may attempt to optimise the code or try a different approach by coding it from scratch.
+// Fixed function:
+function myFirstKata(a, b) {
+  if (typeof(a) !== "number" || typeof(b) !== "number") {
+    return false;
+  } else {
+    return (a % b) + (b % a)
+  }
+}
+// Optimized function:
+function myFirstKata(a, b) {
+  return typeof a !== "number" || typeof b  !== "number" ? false : (a % b) + (b % a)
+}
+// Nice little stroll around the block.
