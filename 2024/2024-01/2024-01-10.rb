246e804583551bc5e0b080e32742cbb6b299b6f8
femmestemgem <femmestemgem@gmail.com>
2024-01-10 15:48:51 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-10.rb
+++ 2024/2024-01/2024-01-10.rb
@@ -12,3 +12,12 @@ end
 
 # print count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
 # I really like my solution the most out of all of them. Booyah.
+
+
+# Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+# Note: input will never be an empty string
+
+def fake_bin(s)
+  s.gsub(/[0-4]/,'0').gsub(/[5-9]/, '1')
+end
+# This was taking way too long until I realized that the test cases were all positive numbers, lol.
