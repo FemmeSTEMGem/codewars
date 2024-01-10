e25ac4ad1c24cdb39ecbd6306af9bf7d04946925
femmestemgem <femmestemgem@gmail.com>
2024-01-10 15:48:19 -0500

    Daily fundamentals practice

---
+++ 2024/2024-01/2024-01-10.rb
@@ -0,0 +1,14 @@
+# Given an array of integers.
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+# If the input is an empty array or is null, return an empty array.
+# Example
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+def count_positives_sum_negatives(lst)
+  lst == [] ? [] :
+  [lst.select { |num| num.positive? }.length, 
+  lst.select { |num| num.negative? }.sum]
+end
+
+# print count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+# I really like my solution the most out of all of them. Booyah.
