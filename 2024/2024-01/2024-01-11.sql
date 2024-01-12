953a86cc0a6071eb865940ef62eada0cc13dd9f5
femmestemgem <femmestemgem@gmail.com>
2024-01-11 21:59:57 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-11.sql
+++ 2024/2024-01/2024-01-11.sql
@@ -12,3 +12,18 @@ FROM
 ORDER BY
   employees DESC
 -- Smoother and smoother each time!
+
+
+-- Given the following table 'decimals':
+-- ** decimals table schema **
+--     id
+--     number1
+--     number2
+-- Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+
+SELECT
+  FLOOR(number1) AS number1,
+  CEILING(number2) AS number2
+FROM
+  decimals
+-- Easy peasy, but make a couple of small silly mistakes that kept me on my toes. Forgot the comma and accidentally shortened CEILING to CEIL (which I think is what it is in either Ruby and/or JavaScript)
