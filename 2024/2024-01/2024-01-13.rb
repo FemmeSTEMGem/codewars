7d2c4ce29e6fc7e9378dcd91cfb62475143b931f
femmestemgem <femmestemgem@gmail.com>
2024-01-13 23:54:24 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-13.rb
+++ 2024/2024-01/2024-01-13.rb
@@ -5,3 +5,11 @@
 def check(arr,element)
   arr.include?(element)
 end
+
+
+# Write a function which converts the input string to uppercase.
+
+def make_upper_case(str)
+  str.upcase
+end
+# Easy peasy. At first did .toUpperCase...but I guess that's JavaScript, lol. ^_^
