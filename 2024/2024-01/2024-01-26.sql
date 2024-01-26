53bbc0161b1b2278b7f3d800b3208e1a49a4f60b
femmestemgem <femmestemgem@gmail.com>
2024-01-26 16:21:21 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-26.sql
+++ 2024/2024-01/2024-01-26.sql
@@ -16,3 +16,18 @@ SELECT
 FROM
   monsters
 -- I did NOT have the brains for this, so I'm incredibly glad that there was a dedicated method for this in SQL. T_T
+
+
+-- Given the following table 'decimals':
+-- ** decimals table schema **
+--     id
+--     number1
+--     number2
+-- Return a table with two columns (root, log) where the values in root are the square root of those provided in number1 and the values in log are changed to a base 10 logarithm from those in number2.
+
+SELECT
+  SQRT(number1) AS root,
+  LOG(number2) AS log
+FROM
+  decimals
+-- As long as SQL has these dedicated math methods, I'm a happy gal.
