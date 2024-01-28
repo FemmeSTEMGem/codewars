f356974ed4c84f6b17c494415c856d219ac1b262
femmestemgem <femmestemgem@gmail.com>
2024-01-27 22:42:05 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-27.js
+++ 2024/2024-01/2024-01-27.js
@@ -28,3 +28,21 @@ console.log(cannonsReady(b))
 // const cannonsReady = (gunners) => {
 //   return Object.values(gunners).includes('nay')?'Shiver me timbers!':'Fire!';
 // }
+
+
+// According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
+// You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
+
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+      return [new Man, new Woman];
+    }
+}
+
+class Human{}
+class Man extends Human{}
+class Woman extends Human{}
+// Definitely needed to look up some things about classes again; haven't dealt with them in a while.
