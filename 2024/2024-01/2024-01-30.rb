0ef97e2751b2bc03c10eb7e4801b62781ffe9584
femmestemgem <femmestemgem@gmail.com>
2024-01-30 22:05:45 -0500

    Daily fundamentals practice

--- 2024/2024-01/2024-01-30.rb
+++ 2024/2024-01/2024-01-30.rb
@@ -7,3 +7,12 @@ def solution(sentence)
   sentence.split(" ").reverse().join(" ")
 end
 # Easy peasy - though I just learned that I could have omitted the "" for .split() because I guess the default behaviour is to split at the spaces. Very cool!
+
+
+# Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+# Can you help her?
+
+def greet(name)
+  name == "Johnny" ? "Hello, my love!" : "Hello, #{name}"
+end
+# Easy peasy - love ternary operations
